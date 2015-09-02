f609b921bf09ae963ff45785b41e88ae00164712
qianyu668899 <jinggeqianyu1991@gmail.com>
2015-09-02 18:41:37 -0400

    Update js structure

--- pyramidgo/static/test.js
+++ pyramidgo/static/test.js
@@ -5,10 +5,6 @@
 function init() {
   var down = "mousedown"; 
   var up = "mouseup";
-  //Add event listener for button 'New  Game'
-  //var newgame = document.getElementsByTagName("newgame");
-  //newgame.addEventListener(up, newGame, false);
-  //document.querySelector("input.button").addEventListener(up, newGame, false);
   var squares = document.getElementsByTagName("td");
   for (var s = 0; s < squares.length; s++) {
     squares[s].addEventListener(down, function(evt){squareSelected(evt, getCurrentPlayer());}, false);
@@ -32,6 +28,39 @@ function initGame() {
   xmlHttp.send(null);
 }
 
+function updateBoard(index) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+          var result = JSON.parse(xmlHttp.responseText);
+          //judge winner
+          check_winner(result._winner);
+          //switch player
+          switch_player(result._current_player)
+        }
+   }
+  var url = "http://localhost:6543/api/update?index="+index;
+  xmlHttp.open('GET', url, true);
+  xmlHttp.send(null);
+}
+
+function history(){
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() { 
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      var history = JSON.parse(xmlHttp.responseText);
+      length = history._history.length;
+      for(var i=0; i<length; i++) {
+        insertTable('h'+i, history._history[i]._game_board, history._history[i]._end_time)
+      }
+    }
+   }
+  var url = "http://localhost:6543/api/history";
+  xmlHttp.open('GET', url, true);
+  xmlHttp.send(null);
+}
+
+/**/
 function createBoard(board) {
   //document.getElementById("debug").innerHTML = "Create Board";
   for(var i = 0; i < board.length; i++) {
@@ -61,11 +90,10 @@ function setInitialPlayer(currentPlayer) {
 function newGame() {
   if (confirm("Start a new game?")) {
     initGame();
-    //document.getElementById("debug").innerHTML = "Start a new game";
   }
 }
 
-/*** call this function whenever a square is clicked or tapped ***/
+
 function squareSelected(evt, currentPlayer) {
   var square = evt.target;
   /* check to see if the square already contains an X or O marker */
@@ -73,7 +101,6 @@ function squareSelected(evt, currentPlayer) {
     alert("Sorry, that space is taken!  Please choose another square.");
     return;
   }
-  /* if not already marked, mark the square, update the array that tracks our board, check for a winner, and switch players */
   else {
     //Call API service to update game state
     fillSquareWithMarker(square, currentPlayer);
@@ -92,23 +119,6 @@ function fillSquareWithMarker(square, player) {
   var marker = document.createElement('div');
   marker.className = player + "-marker";
   square.appendChild(marker);
-}
-
-/*** update our array which tracks the state of the board, and write the current state to local storage ***/
-function updateBoard(index) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-          var result = JSON.parse(xmlHttp.responseText);
-          //judge winner
-          check_winner(result._winner);
-          //switch player
-          switch_player(result._current_player)
-        }
-   }
-  var url = "http://localhost:6543/api/update?index="+index;
-  xmlHttp.open('GET', url, true);
-  xmlHttp.send(null);
 }
 
 function check_winner(winner){
@@ -141,21 +151,6 @@ function remove_Event_Listener(){
   } 
 }
 
-function history(){
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function() { 
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      var history = JSON.parse(xmlHttp.responseText);
-      length = history._history.length;
-      for(var i=0; i<length; i++) {
-        insertTable('h'+i, history._history[i]._game_board, history._history[i]._end_time)
-      }
-    }
-   }
-  var url = "http://localhost:6543/api/history";
-  xmlHttp.open('GET', url, true);
-  xmlHttp.send(null);
-}
 
 function insertTable(cell, board, time)
 {
