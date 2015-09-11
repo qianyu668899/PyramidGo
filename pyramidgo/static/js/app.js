1d03713efdb8f1580fcd32e7008342c98d81d58c
yqian1991 <jinggeqianyu1991@gmail.com>
2015-09-11 10:15:03 -0400

    Add game board and other components

--- pyramidgo/static/js/app.js
+++ pyramidgo/static/js/app.js
@@ -1,5 +1,6 @@
 var how_to_play = "Introduction, ................................................................................................................................."
 
+/* ADD LISTENERS FOR ALL EVENTS*/
 $("#how_to_play").click(function(){
     if($(this).text().trim() == 'How to Play' ){
         $(this).text("Hide message");
@@ -10,24 +11,65 @@ $("#how_to_play").click(function(){
     }
 })
 
+$("#new_game").click(function(){
+    init();
+})
+
+$("#warning_alert").hide();
+
+$("#board td").on(
+   'mousedown', function(evt) {
+     return squareSelected(evt, getCurrentPlayer());
+   }
+);
+
 function init() {
-  $("table.d td").on(
-       'mouseenter', function(evt) {
-           squareSelected(evt);
-       }
-  );
-  //initGame();
+　//set initial player to X
+  $("#current_player").text("X");
+  createBoard();
 }
 
-function squareSelected(evt) {
+/* Control game flow*/
+function createBoard() {
+  //document.getElementById("debug").innerHTML = "Create Board";
+   $("#board td").each(function(){
+     $(this).text("");
+   });
+}
+
+function squareSelected(evt, current_player) {
+  $("#warning_alert").hide();
   var square = evt.target;
   /* check to see if the square already contains an X or O marker */
   if (square.className.match(/marker/)) {
-    alert("Sorry, that space is taken!  Please choose another square.");
+    $("#warning_alert").show();
+    $("#warning").text("Sorry,that space is taken!");
+    //alert("Sorry, that space is taken!  Please choose another square.");
     return;
   }
   else {
-    alert("selected one");
+    fillSquareWithMarker(square, current_player);
+    switch_player(current_player);
     return;
+  }
+}
+
+function fillSquareWithMarker(square, player) {
+  var marker = document.createElement('div');
+  marker.className = player + "-marker";
+  square.appendChild(marker);
+}
+
+function getCurrentPlayer() {
+  current_player = $("#current_player").text();
+  return current_player;
+}
+
+function switch_player(current_player){
+  if (current_player == "X" ) {
+    $("#current_player").text("O");
+  }
+  else {
+    $("#current_player").text("X");
   }
 }
